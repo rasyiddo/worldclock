794e2d977baefd3a71453c6aa88fc8cc58850ea2
rasyiddo <sulaimanrasyiddinitra7@gmail.com>
2026-09-22 23:13:03 +0700

    feat: savedcity modified

--- app/src/main/java/com/example/worldclock/SavedCity.kt
+++ app/src/main/java/com/example/worldclock/SavedCity.kt
@@ -6,12 +6,14 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "saved_cities")
 data class SavedCity(
 
-    @PrimaryKey
     val timezone: String,
 
     val city: String,
 
     val country: String,
 
-    val flag: String
+    val flag: String,
+
+    @PrimaryKey
+    val id: String = "$country|$city|$timezone"
 )
